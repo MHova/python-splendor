ad0239eb3def360e64e7f114a15013215cb4c6a7
MHova <mho@sumall.com>
2016-06-09 23:46:29 -0700

    Implement player gem state

--- splendor.py
+++ splendor.py
@@ -5,6 +5,10 @@ import copy
 
 GEM_COLORS = {'black', 'blue', 'green', 'red', 'white', 'yellow'}
 
+class Player:
+  def __init__(self):
+    self.gems = {c : 0 for c in GEM_COLORS}
+
 def determine_starting_gems(num_players):
   if num_players < 2 or num_players > 4:
     raise ValueError('There must be 2-4 players')
@@ -31,8 +35,9 @@ def dict_to_pretty_str(d):
   format_string = '{}:{}'
   return separator.join(format_string.format(k, v) for k, v in d.items())
 
-def help_string(gems):
-  current_state_string = 'Available gems - ' + dict_to_pretty_str(gems)
+def help_string(gems, player):
+  current_state_string = 'Available gems - ' + dict_to_pretty_str(gems) + '\n\n'
+  current_state_string += 'Player currently has - ' + dict_to_pretty_str(player.gems)
 
   help_string = '''Possible Actions:
     1) take <color1> <color2> <color3> - Take 3 gems, each of different colors. ex: take black blue red
@@ -40,7 +45,7 @@ def help_string(gems):
 
   return current_state_string + '\n\n' + help_string + '\n'
 
-def take_different_gems(colors_list, gems_state):
+def take_different_gems(colors_list, gems_state, player):
   for color in colors_list:
     if color not in GEM_COLORS:
       print('{} is not a valid color'.format(color))
@@ -64,18 +69,22 @@ def take_different_gems(colors_list, gems_state):
   taken_gems_str = ', '.join(c for c in colors_list)
   print('You took {} gems'.format(taken_gems_str))
 
-  new_state = copy.deepcopy(gems_state)
+  new_gems_state = copy.deepcopy(gems_state)
+  new_player = copy.deepcopy(player)
   for color in colors_list:
-    new_state[color] = new_state[color] - 1
-  return new_state
+    new_gems_state[color] = new_gems_state[color] - 1
+    new_player.gems[color] = new_player.gems[color] + 1
+  return (new_gems_state, new_player)
 
 # main
 num_players = input("Enter the number of players: ")
 gems_state = determine_starting_gems(int(num_players))
+# TODO: implement multiple players
+player = Player()
 print("The starting gems will be " + dict_to_pretty_str(gems_state) + '\n')
 
 while True:
-  print(help_string(gems_state))
+  print(help_string(gems_state, player))
   action = input('Enter an action\n').strip()
 
   if action.lower() == 'quit':
@@ -86,6 +95,10 @@ while True:
   match = re.search(take_different_gems_pattern, action)
 
   if match:
-    gems_state = take_different_gems(
-                  [match.group(1), match.group(2), match.group(3)], gems_state)
+    result = take_different_gems(
+              [match.group(1), match.group(2), match.group(3)],
+              gems_state,
+              player)
+    gems_state = result[0]
+    player = result[1]
 
